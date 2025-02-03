832d1ac1bba6d096b40f63e625cb584505ee9d87
Mohamed Asmali <asmali@Mohameds-MacBook-Pro-2.local>
2025-02-02 23:32:56 -0500

    reorganized files and put company collabs

--- js/script.js
+++ js/script.js
@@ -1,16 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const animationArea = document.querySelector(".animation-area");
+    const sponsorTrack = document.querySelector(".sponsor-track div");
     
-    for (let i = 0; i < 15; i++) {
-      let bubble = document.createElement("div");
-      bubble.classList.add("bubble");
-      animationArea.appendChild(bubble);
-      
-      const size = Math.random() * 80 + 20;
-      bubble.style.width = `${size}px`;
-      bubble.style.height = `${size}px`;
-      bubble.style.left = `${Math.random() * 100}vw`;
-      bubble.style.animationDuration = `${Math.random() * 6 + 6}s`;
-    }
+    // Duplicate the sponsor images for seamless scrolling
+    const duplicateSponsors = sponsorTrack.cloneNode(true);
+    sponsorTrack.parentNode.appendChild(duplicateSponsors);
   });
   
